e19779cc7e3d19f6eeaf5a28da64c75a047e77f5
Charlie <30829462+Charlie-Hill@users.noreply.github.com>
2026-02-17 18:14:22 +0000

    Update astro.config for Astro 5.17.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
@@ -7,48 +7,52 @@ export default defineConfig({
   },
   experimental: {
     fonts: [{
-      provider: "local",
+      provider: fontProviders.local(),
       name: "InterLocal",
       cssVariable: "--font-inter",
-      variants: [
-        {
-          weight: 400,
-          style: "normal",
-          src: ["./src/assets/fonts/Inter-Regular.woff2"]
-        },
-        {
-          weight: 600,
-          style: "normal",
-          src: ["./src/assets/fonts/Inter-SemiBold.woff2"]
-        },
-        {
-          weight: 700,
-          style: "normal",
-          src: ["./src/assets/fonts/Inter-Bold.woff2"]
-        }
-      ]
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/Inter-Regular.woff2"]
+          },
+          {
+            weight: 600,
+            style: "normal",
+            src: ["./src/assets/fonts/Inter-SemiBold.woff2"]
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ["./src/assets/fonts/Inter-Bold.woff2"]
+          }]
+      }
     },
     {
-      provider: "local",
+      provider: fontProviders.local(),
       name: "InterLocalDisplay",
       cssVariable: "--font-inter-display",
-      variants: [
-        {
-          weight: 400,
-          style: "normal",
-          src: ["./src/assets/fonts/InterDisplay-Regular.woff2"]
-        },
-        {
-          weight: 500,
-          style: "normal",
-          src: ["./src/assets/fonts/InterDisplay-Medium.woff2"]
-        },
-        {
-          weight: 600,
-          style: "normal",
-          src: ["./src/assets/fonts/InterDisplay-SemiBold.woff2"]
-        }
-      ]
+
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/InterDisplay-Regular.woff2"]
+          },
+          {
+            weight: 500,
+            style: "normal",
+            src: ["./src/assets/fonts/InterDisplay-Medium.woff2"]
+          },
+          {
+            weight: 600,
+            style: "normal",
+            src: ["./src/assets/fonts/InterDisplay-SemiBold.woff2"]
+          }
+        ]
+      }
     }]
   }
-})
+});
